f02b0f1ecd8f22cf41ae5b85a737d2bb5b598c10
Kevin Wolf <kevinw@software-logistics.com>
2023-11-09 11:31:06 -0500

    changes for device field on planner

--- src/LagoVista.IoT.StarterKit/Managers/AppFactory.cs
+++ src/LagoVista.IoT.StarterKit/Managers/AppFactory.cs
@@ -405,9 +405,9 @@ namespace LagoVista.IoT.StarterKit.Managers
             {
                 Name = "Sample Planner",
                 Key = "sampleplanner",
-                MessageTypeIdParsers = new List<DeviceMessaging.Admin.Models.DeviceMessageDefinitionField>()
+                MessageTypeIdParsers = new List<DeviceMessaging.Admin.Models.DeviceField>()
                 {messageIdParser1, messageIdParser2},
-                DeviceIdParsers = new List<DeviceMessaging.Admin.Models.DeviceMessageDefinitionField>()
+                DeviceIdParsers = new List<DeviceMessaging.Admin.Models.DeviceField>()
                 {deviceIdParser1, deviceIdParser2}
             };
             AddId(planner);
